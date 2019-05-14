99662ca28984a58de7a04d0aa6fbd7d23ee51994
Konrad Kuceł <konradkucel@gmail.com>
2019-05-14 17:46:38 +0200

    Add scroll event, slide progress bar and Reset button

--- js/script.js
+++ js/script.js
@@ -3,8 +3,17 @@
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
-  cellAlign: 'left',
-  contain: true
+  cellAlign: 'center',
+  contain: true,
+  pageDots: false,
+  hash: true
+});
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
 });
 
 // element argument can be a selector string
